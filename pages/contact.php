f1d5490ab1b9823609ff75bd0a63e8441be7170b
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-02-13 14:54:34 +0100

    #4 form issue

--- pages/contact.php
+++ pages/contact.php
@@ -1,23 +1,26 @@
 <?php include"../views/header.php"?>
-<div class="d-flex justify-content-center align-content-center bg-danger">
+<div class="d-flex justify-content-center align-content-center bg-danger align-items-stretch">
     <form method="post" action="./contact.php">
         <h3>Vous souhaitez contacter<?= $_POST["developper"] ?></h3>
         <label>Nom :<br>
-            <input type="text" name="name">
+            <input type="text" name="last name">
         </label>
         <label>Prénom :<br>
-            <input type="text" name="name">
+            <input type="text" name="first name">
         </label>
         <br>
-        <label class="d-flex align-center flex-row">email :<br>
-            <input type="text" name="name">
+        <label class="d-flex align-center flex-column">Email :<br>
+            <input type="email" name="email">
         </label>
         <br>
         <label>Téléphone :<br>
-            <input type="text" name="name">
+            <input type="number" name="phone">
         </label>
         <br>
-    <button>Envoyer</button>
+        <div class="d-flex flex-row justify-content-center">
+            <button>Envoyer</button>
+        </div>
+
     </form>
 </div>
 <?php /*
