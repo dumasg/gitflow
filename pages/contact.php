aca25296d254f1e278fa5720038528c17e71ee69
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-02-13 14:37:38 +0100

    adding boostrap class, work in progress

--- pages/contact.php
+++ pages/contact.php
@@ -1,20 +1,22 @@
-<div class="d-flex justify-content-center align-content-center">
+<?php include"../views/header.php"?>
+<div class="d-flex justify-content-center align-content-center bg-danger">
     <form method="post" action="./contact.php">
         <h3>Vous souhaitez contacter<?= $_POST["developper"] ?></h3>
-        <label>Nom :
+        <label>Nom :<br>
             <input type="text" name="name">
         </label>
-        <label>Prénom :
-            <input type="text" name="name">
-        </label>
-        <br>
-        <label>email :
+        <label>Prénom :<br>
             <input type="text" name="name">
         </label>
         <br>
-        <label>Téléphone :
+        <label class="d-flex align-center flex-row">email :<br>
             <input type="text" name="name">
         </label>
+        <br>
+        <label>Téléphone :<br>
+            <input type="text" name="name">
+        </label>
+        <br>
     <button>Envoyer</button>
     </form>
 </div>
@@ -24,3 +26,4 @@
  <input type="submit" value="Envoyer">
  </form>
  */
+include "../views/footer.php";
