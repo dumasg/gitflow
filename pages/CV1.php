e0889c555e17fadb5570bb48f028e1234d446502
Eddy <eddy.vernet@le-campus-numerique.fr>
2024-02-13 14:30:28 +0100

    Cv1 partie 1

---
+++ pages/CV1.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CV</title>
+</head>
+<body>
+
+<?php include '../views/header.php' ?>
+
+<h1> Curricullum Vitae</h1>
+
+<h3>À propos de moi</h3>
+
+<span class="text-bg-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus eros
+    felis, a laoreet turpis sollicitudin vitae. Proin aliquet venenatis mauris
+    vitae hendrerit. Integer ligula dolor, fringilla quis massa hendrerit, lobortis
+    ultricies urna. Aenean et risus massa. Cras imperdiet aliquam finibus.
+    Sed ultrices odio sed molestie varius. Curabitur malesuada diam a elit ullamcorper
+    venenatis. Maecenas et blandit nunc. In hac habitasse platea dictumst.
+    Maecenas suscipit ultricies neque, pulvinar molestie arcu mollis quis.
+    Duis rutrum rutrum accumsan. Nullam sed risus nunc. Aliquam nec consectetur urna,
+    nec porttitor tortor.
+
+Proin euismod sem scelerisque, condimentum neque non, ultrices tellus. Nullam
+    turpis libero, rutrum eu lobortis non, aliquam nec nisi. Sed sapien nisi,
+    porta tincidunt tellus et, cursus sagittis nulla. Sed eu massa blandit ante
+    mattis sagittis. Ut vulputate vulputate nunc. Fusce a urna et orci tristique
+    tempor sed id dolor. Phasellus pretium eros sed augue egestas, id pellentesque
+    velit luctus. Fusce sollicitudin, ligula id auctor luctus, ligula neque tempor
+    orci, quis commodo ligula dolor tincidunt metus. Curabitur feugiat aliquet velit
+    in ultrices. Donec nunc tellus, rhoncus quis tempor sit amet, aliquam id lacus.
+    Donec quis rhoncus erat. Donec dignissim porta tellus, sed posuere velit fermentum
+    nec. Pellentesque tincidunt sodales orci non euismod. </span>
+
+
+
+
+
+
+
+<?php include '../views/footer.php' ?>
+</body>
+</html>
